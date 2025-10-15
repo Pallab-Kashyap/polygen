858b66c0db78ce0c9d1c8d2d9153b5ba1eea5d09
Pallab <pallabkshyp@gmail.com>
2025-10-15 18:40:30 +0530

    Emiail service bug solved

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,5 +1,5 @@
 export const EMAIL_CONFIG = {
-  recipientEmail: process.env.RECIPIENT_EMAIL,
+  recipientEmail: process.env.RECIPIENT_EMAIL || "info@polygen.co.in",
 
   fromEmail: "Polygen Website <noreply@polygen.co.in>",
 
